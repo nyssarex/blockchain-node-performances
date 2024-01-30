95e6f69332c1122b760cd4ce72a87578b6ec0b4a
Amir Nyssambayev <69467191+nyssarex@users.noreply.github.com>
2024-01-30 02:01:25 +0000

    mvp code, next add options

--- pipeline.py
+++ pipeline.py
@@ -3,7 +3,8 @@ from web3 import Web3, AsyncWeb3
 
 
 secret_key = os.environ.get('CHAINSTACK')
-print(secret_key)
-#w3 = Web3(Web3.HTTPProvider(secret_key))
 
-#print(w3.eth.get_block('latest'))
+w3 = Web3(Web3.HTTPProvider(secret_key))
+
+block = w3.eth.get_block('latest')
+
